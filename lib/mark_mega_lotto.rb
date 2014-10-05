7be3439dc2a4908d362576525cfc82e1e005d7f7
Mark Chavez <markchav3z@gmail.com>
2014-10-05 08:29:34 +0800

    initial commit

---
+++ lib/mark_mega_lotto.rb
@@ -0,0 +1,6 @@
+require "mark_mega_lotto/version"
+require "mark_mega_lotto/drawing"
+
+module MarkMegaLotto
+  # Your code goes here...
+end
---
+++ lib/mark_mega_lotto/version.rb
@@ -0,0 +1,3 @@
+module MarkMegaLotto
+  VERSION = "0.0.1"
+end
